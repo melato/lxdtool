6c5d641251ccd6265ba8180735fc4bf86597c5e3
Alex Athanasopoulos <alex@melato.org>
2018-12-03 11:30:35 +0200

    changed copyright format to something that is not "corrected" by LiteIDE

--- cmd/SnapCmd.go
+++ cmd/SnapCmd.go
@@ -1,6 +1,5 @@
-/* SPDX-License-Identifier: Apache-2.0
-*  Copyright 2018 Alex Athanasopoulos
- */
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2018 Alex Athanasopoulos
 package cmd
 
 import (
